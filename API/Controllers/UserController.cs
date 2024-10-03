9cffcb98e3680d6937977a84aa4aa35f51a022fe
AnasAmin <anasm@mkcl.com.eg>
2024-10-03 16:25:26 +0300

    d

---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using Application.Implementations.IdentityServices;
+using Application.Interfaces.IIdentityServices;
+using Microsoft.AspNetCore.Mvc;
+using Shared.Common.ApiRoutes;
+using Shared.Common.General;
+using System.Net;
+
+namespace API.Controllers
+{
+    public class UserController : BaseController
+    {
+        private readonly IUserService _userService;
+        public UserController(IHttpContextAccessor httpContextAccessor, IUserService userService) : base(httpContextAccessor)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet]
+        [Route(ApiRoutes.User.GetAll)]
+        public async Task<IActionResult> GetAllUsers([FromQuery] PaginationSearchModel queryParams, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var result = await _userService.GetAllUsersAsync(queryParams, cancellationToken);
+                return ReturnResult(result);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+
+    }
+}
