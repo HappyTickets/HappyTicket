dc73f535f4bdbf0bf43d2fc12826d4b87ff3ae64
Mohamed Ahmed <mo.19.ahmed.47@gmail.com>
2024-10-10 15:08:32 +0300

    feat: Complete User-Roles Management with CRUD, Multi-Select User Assignments, and Fluent Validations

---
+++ Application/Validation/RolesValidator/EditRoleValidator.cs
@@ -0,0 +1,56 @@
+﻿using Domain.Entities.UserEntities.AuthEntities;
+using FluentValidation;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Localization;
+using Shared.DTOs.Authorization.Request;
+using Shared.ResourceFiles;
+
+namespace Application.Validation.RolesValidator
+{
+    public class EditRoleValidator : AbstractValidator<EditRoleDto>
+    {
+        #region Fields
+        private readonly IStringLocalizer<Resource> _stringLocalizer;
+        private readonly RoleManager<Role> _roleManager;
+        #endregion
+        #region Constructors
+
+        #endregion
+        public EditRoleValidator(IStringLocalizer<Resource> stringLocalizer
+                                 , RoleManager<Role> roleManager)
+        {
+            _stringLocalizer = stringLocalizer;
+            _roleManager = roleManager;
+            ApplyValidationsRules();
+        }
+
+        #region Actions
+        public void ApplyValidationsRules()
+        {
+            RuleFor(x => x.RoleId)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage(_stringLocalizer[Resource.RequiredFields]);
+        }
+
+
+        public void ApplyCustomValidationsRules()
+        {
+            RuleFor(x => x.RoleName)
+             .NotEmpty().WithMessage(_stringLocalizer[Resource.RequiredFields])
+             .When(x => !string.IsNullOrEmpty(x.RoleName))
+             .MustAsync(async (dto, roleName, cancellationToken) =>
+             {
+                 var role = await _roleManager.FindByNameAsync(roleName);
+                 return role == null || role.Id == dto.RoleId;
+             })
+             .WithMessage(_stringLocalizer[Resource.RoleAlreadyExists])
+             .When(x => !string.IsNullOrEmpty(x.RoleName));
+
+        }
+
+
+
+        #endregion
+    }
+}
---
+++ Client/Services/Implementation/BAuthorizationService.cs
@@ -0,0 +1,83 @@
+﻿using Client.Services.Interfaces;
+using LanguageExt;
+using LanguageExt.Common;
+using Shared.Common;
+using Shared.Common.ApiRoutes;
+using Shared.Common.General;
+using Shared.DTOs.Authorization.Request;
+using Shared.DTOs.Authorization.Response;
+
+namespace Client.Services.Implementation
+{
+    public class BAuthorizationService : BIAuthorizationService
+    {
+        private readonly IHttpClientHelper _httpClient;
+        private const string BASE = ApiRoutes.Authorization.Base;
+
+        public BAuthorizationService(IHttpClientHelper httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<Result<BaseResponse<Unit>>> AddRoleAsync(AddRoleDto addRoleDto)
+        {
+            return await _httpClient.PostBaseAsync<AddRoleDto, BaseResponse<Unit>>(BASE + ApiRoutes.Authorization.CreateRole, addRoleDto);
+        }
+
+        public async Task<Result<BaseResponse<Unit>>> EditRoleAsync(EditRoleDto request)
+        {
+            return await _httpClient.PostBaseAsync<EditRoleDto, BaseResponse<Unit>>(BASE + ApiRoutes.Authorization.EditRole, request);
+        }
+
+        public async Task<Result<BaseResponse<Unit>>> DeleteRoleAsync(string roleId)
+        {
+            return await _httpClient.GetBaseAsync<BaseResponse<Unit>>(BASE + ApiRoutes.Authorization.DeleteRole.Replace("{id}", roleId));
+        }
+
+        public async Task<Result<BaseResponse<List<RoleDto>>>> GetRolesList(CancellationToken cancellationToken)
+        {
+            return await _httpClient.GetBaseAsync<BaseResponse<List<RoleDto>>>(BASE + ApiRoutes.Authorization.RolesList);
+        }
+
+        public async Task<Result<BaseResponse<RoleDto>>> GetRoleById(string id)
+        {
+            return await _httpClient.GetBaseAsync<BaseResponse<RoleDto>>(BASE + ApiRoutes.Authorization.GetRoleById.Replace("{id}", id));
+        }
+
+        public async Task<Result<BaseResponse<Unit>>> AssignUserToRolesAsync(AssignUserToRolesDto assignUserToRoleDto)
+        {
+            return await _httpClient.PostBaseAsync<AssignUserToRolesDto, BaseResponse<Unit>>(BASE + ApiRoutes.Authorization.AssignUserToRoles, assignUserToRoleDto);
+        }
+        public async Task<Result<BaseResponse<Unit>>> AssignUsersToRoleAsync(AssignUsersToRoleDto assignUsersToRoleDto, CancellationToken cancellationToken)
+        {
+            return await _httpClient.PostBaseAsync<AssignUsersToRoleDto, BaseResponse<Unit>>(BASE + ApiRoutes.Authorization.AssignUsersToRole, assignUsersToRoleDto);
+        }
+        public async Task<Result<BaseResponse<Unit>>> RemoveUsersFromRoleAsync(RemoveUsersFromRoleDto removeUserFromRoleDto, CancellationToken cancellationToken)
+        {
+            return await _httpClient.PostBaseAsync<RemoveUsersFromRoleDto, BaseResponse<Unit>>(BASE + ApiRoutes.Authorization.RemoveUsersFromRole, removeUserFromRoleDto);
+        }
+
+        public async Task<Result<BaseResponse<UserWithRolesDto>>> GetUserWithRolesAsync(string userId)
+        {
+            return await _httpClient.GetBaseAsync<BaseResponse<UserWithRolesDto>>(BASE + ApiRoutes.Authorization.GetUserWithRoles.Replace("{userId}", userId));
+        }
+
+        public async Task<Result<BaseResponse<RoleWithUsersDto>>> GetRoleWithUsersAsync(string roleId, PaginationSearchModel paginationSearchModel, CancellationToken cancellationToken = default)
+        {
+            var queryParameters = paginationSearchModel.AsDictionary();
+
+            return await _httpClient.GetBaseAsync<BaseResponse<RoleWithUsersDto>>(BASE + ApiRoutes.Authorization.GetRoleWithUsers.Replace("{roleId}", roleId), queryParameters);
+        }
+
+        public async Task<Result<BaseResponse<PaginatedList<UserWithRolesDto>>>> GetUsersWithRolesAsync(PaginationSearchModel paginationSearchModel, CancellationToken cancellationToken)
+        {
+            var queryParameters = paginationSearchModel.AsDictionary();
+
+            return await _httpClient.GetBaseAsync<BaseResponse<PaginatedList<UserWithRolesDto>>>
+
+                (BASE + ApiRoutes.Authorization.GetUsersWithRoles, queryParameters);
+        }
+
+
+    }
+}
