8cc8ebd397a51ad533d44c3b416c71db0fdf44ea
TarekNeGm <60225477+TarekNeGm@users.noreply.github.com>
2024-10-11 19:45:45 -0800

    Committed By Tarek NeGm

---
+++ Client/Pages/Company/ContactUs.razor.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using Shared.DTOs;
+
+namespace Client.Pages.Company
+{
+    public partial class ContactUs
+    {
+        private CustomerInfoDto CustomerInfoDto { get; set; } = new();
+
+        [CascadingParameter]
+        public Task<AuthenticationState>? AuthenticationState { get; set; }
+
+        protected override async Task OnParametersSetAsync()
+        {
+            var user = (await AuthenticationState!).User;
+        }
+
+        private bool IsFormValidToProceedWith()
+        {
+            var result = !string.IsNullOrWhiteSpace(CustomerInfoDto.Name) &&
+                         !string.IsNullOrWhiteSpace(CustomerInfoDto.Email) &&
+                         !string.IsNullOrWhiteSpace(CustomerInfoDto.Message);
+
+            return result;
+        }
+
+        private async Task SendCustomerRequest()
+        {
+            Console.WriteLine("Message Sent!");
+        }
+    }
+}
