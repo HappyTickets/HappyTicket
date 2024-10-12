788a714c539e2c25c99adc55d906e3b6ac7dc18b
TarekNeGm <60225477+TarekNeGm@users.noreply.github.com>
2024-10-11 20:44:21 -0800

    SMTP Send Message to Contact Us Service Is Done

---
+++ Domain/Entities/ContactEntity/Contact.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Entities.ContactEntity
+{
+    public class Contact
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Note { get; set; }
+    }
+
+}
---
+++ Domain/Entities/ContactEntity/SmtpSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Entities.ContactEntity
+{
+    public class SmtpSettings
+    {
+        public string Host { get; set; }
+        public int Port { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+
+}
