1a09e80b1ab4a12092ac10317562efada97e5179
hema325 <hema24790@gmail.com>
2024-10-20 18:04:56 +0300

    Change total items from int to long

--- Infrastructure/Persistence/Extensions/PaginationExtensions.cs
+++ Infrastructure/Persistence/Extensions/PaginationExtensions.cs
@@ -8,7 +8,7 @@ namespace Infrastructure.Persistence.Extensions
         public async static Task<PaginatedList<TEntity>> PaginateAsync<TEntity>(this IQueryable<TEntity> query,int pageIndex, int pageSize)
         {
             var items = await query.Skip(pageIndex * pageSize).Take(pageSize).ToListAsync();
-            var count = await query.CountAsync();
+            var count = await query.LongCountAsync();
 
             return new PaginatedList<TEntity>(items, count, pageIndex, pageSize);
         }
--- Shared/Common/General/PaginatedList.cs
+++ Shared/Common/General/PaginatedList.cs
@@ -5,11 +5,11 @@
         public IEnumerable<T> Items { get; set; }
         public int PageIndex { get; set; }
         public int TotalPages { get; set; }
-        public int TotalItems { get; set; }
+        public long TotalItems { get; set; }
         public bool HasPrevious => PageIndex > 0;
         public bool HasNext => PageIndex < TotalPages - 1;
 
-        public PaginatedList(IEnumerable<T> items, int count, int pageIndex, int pageSize)
+        public PaginatedList(IEnumerable<T> items, long count, int pageIndex, int pageSize)
         {
             PageIndex = pageIndex;
             TotalItems = count;
@@ -17,7 +17,7 @@
             Items = items;
         }
 
-        public static PaginatedList<T> Create(IEnumerable<T> items, int count, int pageIndex, int pageSize)
+        public static PaginatedList<T> Create(IEnumerable<T> items, long count, int pageIndex, int pageSize)
         {
             return new PaginatedList<T>(items, count, pageIndex, pageSize);
         }
