4baf55361d87bdea2303a4e91187d39d87754c58
TarekNeGm <60225477+TarekNeGm@users.noreply.github.com>
2024-10-19 23:53:54 -0800

    Create Validator Is Done

---
+++ Application/Champions/Validators/CreateChampionValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using Shared.DTOs.Champion;
+
+namespace Application.Champions.Validators
+{
+    public class CreateChampionValidator : AbstractValidator<CreateChampionshipDto>
+    {
+        public CreateChampionValidator()
+        {
+            RuleFor(result => result.Name).NotEmpty().WithMessage("Name Is Required");
+            RuleFor(result => result.Logo).NotEmpty().WithMessage("Logo Is Required");
+        }
+    }
+}
