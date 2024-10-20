4458ab62ee63e060af45a30d0177d40d9d55ee57
TarekNeGm <60225477+TarekNeGm@users.noreply.github.com>
2024-10-19 23:56:44 -0800

    Update Validator Is Done

---
+++ Application/Champions/Validators/UpdateChampionshipValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using Shared.DTOs.ChampionDtos;
+
+namespace Application.Champions.Validators
+{
+    public class UpdateChampionshipValidator : AbstractValidator<UpdateChampionshipDto>
+    {
+        public UpdateChampionshipValidator()
+        {
+            RuleFor(result => result.Id).NotEmpty().WithMessage("Id Is Required");
+            RuleFor(result => result.Name).NotEmpty().WithMessage("Name Is Required");
+            RuleFor(result => result.Logo).NotEmpty().WithMessage("Logo Is Required");
+        }
+    }
+}
