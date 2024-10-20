4d38bcb658eebb3f00076209cf7adac51b2548c6
TarekNeGm <60225477+TarekNeGm@users.noreply.github.com>
2024-10-19 22:04:30 -0800

    Commit

---
+++ Application/Champions/Service/IChampionService.cs
@@ -0,0 +1,14 @@
+﻿using Shared.Common;
+using Shared.DTOs.Champion;
+
+namespace Application.Interfaces
+{
+    public interface IChampionService
+    {
+        ValueTask<BaseResponse<ChampionDto>> CreateAsync(CreateOrUpdateChampionDto dto, bool autoSave = true, CancellationToken cancellationToken = default);
+        ValueTask<BaseResponse<ChampionDto>> UpdateAsync(CreateOrUpdateChampionDto dto, CancellationToken cancellationToken = default);
+        //Task<Result<ChampionDto>> DeleteAsync(Guid id, CancellationToken cancellationToken = default);
+        //Task<Result<IEnumerable<ChampionDto>>> GetAllAsync(bool useCache = true, CancellationToken cancellationToken = default);
+        //Task<Result<ChampionDto>> GetByIdAsync(Guid id, bool useCache = true, CancellationToken cancellationToken = default);
+    }
+}
